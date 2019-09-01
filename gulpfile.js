8a479e82fbfedeac06ac59ff669899d2e6d8d870
elyaperagov <piragof@gmail.com>
2019-09-01 07:56:05 +0300

    начал сборку

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,12 @@
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var sourcemap = require("gulp-sourcemaps");
+var rename = require("gulp-rename");
 var less = require("gulp-less");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var csso = require("gulp-csso");
+var imagemin = require("gulp-imagemin");
 var server = require("browser-sync").create();
 
 gulp.task("css", function () {
@@ -16,9 +19,20 @@ gulp.task("css", function () {
     .pipe(postcss([
       autoprefixer()
     ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
     .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
+});
+
+gulp.task("images", function() {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3})
+    ]))
+
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("server", function () {
